e9bb9313a2296703894ce26948c7f3b2e1a0cd0f
nstuli <nusratsiddiquetuli@gmail.com>
2024-12-31 05:33:04 +0600

    ok

--- quizapp/src/app/page.tsx
+++ quizapp/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-6xl font-bold">Hello World👋</h1>
+      <h1 className="text-6xl font-bold">welcome to the quizapp ^ ^ </h1>
     </main>
-  )
+  );
 }
